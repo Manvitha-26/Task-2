6d315f69704bd9f27f555063b1b3d1963fc76c81
Manvitha-26 <98576900+Manvitha-26@users.noreply.github.com>
2022-02-03 15:46:43 +0530

    first commit

---
+++ test_sample_output.py
@@ -0,0 +1,33 @@
+
+E:\python_class\realtime\practice>test_sample.py
+start time for sequenceTraverse 2022-02-03 13:51:08.083033
+dut1
+dut2
+dut3
+dut4
+End time for sequenceTraverse 2022-02-03 13:51:08.084012
+start time for sequenceTraverse 2022-02-03 13:51:08.245975
+dut1
+dut2
+dut3
+dut4
+End time for sequenceTraverse 2022-02-03 13:51:08.246975
+PID for process 1: 45620
+start time in listTraverse: 1643876468.246975
+start time for sequenceTraverse 2022-02-03 13:51:08.252973
+dut1
+dut2
+dut3
+dut4
+End time for sequenceTraverse 2022-02-03 13:51:08.252973
+PID for process 2: 8784
+T1: dut1
+T2: dut4
+T2: dut3
+T1: dut2
+T2: dut2
+T1: dut3
+T2: dut1
+T1: dut4
+total time: 8.02359652519226
+done
